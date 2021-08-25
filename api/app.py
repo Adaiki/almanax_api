e1d5bbd70e078c91034925db2870271ed8699fd8
Turbo <turbo@module>
2021-08-25 02:52:10 +0200

    [EDIT] Use json file instead of mysql database

--- api/app.py
+++ api/app.py
@@ -1,15 +1,32 @@
+# coding: utf-8
+
 import os
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, abort
+from flask_restful import Resource, Api, reqparse
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class AlmanaxResource(Resource):
     def get(self):
-        return {'status': 'api working'}
+        f = open('data/almanax.json')
+        almanax = json.load(f)
+        f.close()
+        return almanax
 
-api.add_resource(HelloWorld, '/')
+class AlmanaxDateResource(Resource):
+    def get(self, date):
+        f = open('data/almanax.json')
+        almanax = json.load(f)
+        f.close()
+
+        if almanax.get(date) == None:
+            abort(404)
+        return almanax.get(date)
+
+api.add_resource(AlmanaxResource, '/almanax/')
+api.add_resource(AlmanaxDateResource, '/almanax/<string:date>')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.getenv('PORT'))
+    app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=True)
